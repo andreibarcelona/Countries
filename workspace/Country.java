2ea592a43cc43401233460e444af605b8223368f
andreibarcelona <zianandrei280@gmail.com>
2025-01-20 21:32:26 +0000

    submission

--- workspace/Country.java
+++ workspace/Country.java
@@ -1,14 +1,55 @@
-public class Country
-{
-  // add private instance variables for the name, capital, language, and image file.
+public class Country {
+  // Private instance variables for the name, capital, language, and image file.
+  private String name;
+  private String capital;
+  private String language;
+  private String image;
 
-  // add constructors
+  // Constructor to initialize a Country object with name, capital, language, and image file.
+  public Country(String initName, String initCapital, String initLanguage, String initImage) {
+    // Pre-condition:
+    // - The parameters initName, initCapital, initLanguage, and initImage are valid, non-null strings.
+    
+    // Set instance variables with the provided values
+    name = initName;
+    capital = initCapital;
+    language = initLanguage;
+    image = initImage;
 
-  // Write accessor/get methods for each instance variable that returns it.
+    // Post-condition:
+    // - The Country object is initialized with the provided name, capital, language, and image file.
+  }
 
-  // Write a toString() method that returns a concatenated String of 3 of the instance variables in a sentence like "..'s capital is .. and its primary language is ..."
- 
+  // Method to return the name of the country.
+  public String getName() {
+    return name;
+  }
 
+  // Method to return the capital of the country.
+  public String getCapital(){
+    return capital;
+  }
 
-  
+  // Method to return the primary language of the country.
+  public String getLanguage(){
+    return name; // Error: should return 'language' instead of 'name'
+  }
+
+  // Method to return the image file associated with the country.
+  public String getImage(){
+    return image;
+  }
+
+  // Method to return a string representation of the country’s information.
+  public String toString() {
+    // Pre-condition:
+    // - The instance variables name, capital, and language are properly initialized.
+    
+    // Concatenate instance variables into a sentence
+    return name + "'s capital is " + capital + " and its primary language is " + language;
+
+    // Post-condition:
+    // - Returns a concatenated string that includes the country name, capital, and primary language.
+    // - The string is in the format "name's capital is capital and its primary language is language."
+  }
 }
--- workspace/Main.java
+++ workspace/Main.java
@@ -4,6 +4,11 @@ import javax.swing.*;
 import java.util.*;
 import java.io.*;
 
+//Andrei Barcelona
+// 1-14-25
+// This app quizzes users on their geogaraphy skills (asks what country is displayed on the map)
+
+
 public class Main 
 {
 
@@ -17,6 +22,7 @@ public class Main
   private ImageIcon img;
   private JLabel imageLabel;
   private JLabel outputLabel;
+  private JTextField input; 
   
   public static void main(String[] args) {
     // Create the GUI
@@ -27,61 +33,136 @@ public class Main
 
   /* loadCountries() reads in the data from the countries-data.csv file and fills in the countryArray with data. You need to add the loop that reads in the country data into the array. */
   public void loadCountries() 
-  {
-    // Open the data file - do not change
-    File file = new File("countries-data.csv");
-    Scanner scan = null;
-    try {
-      scan = new Scanner(file);
-    } catch(FileNotFoundException e) { 
-        System.out.println("File not found");     
-    }
-    
-    // Write a for loop that goes through the countryArray.
-    // for(int i ....) {
+{
+  // Open the data file - do not change
+  File file = new File("/workspaces/Countries/workspace/countries-data.csv");
+  Scanner scan = null;
+  try {
+    scan = new Scanner(file);
+  } catch(FileNotFoundException e) { 
+      System.out.println("File not found");     
+  }
+  
+  // Pre-condition:
+  // - The countryArray is declared and has a predefined size (it must be able to hold data).
+  // - The file "/workspaces/Countries/workspace/countries-data.csv" exists and is accessible.
+  // - The Country constructor accepts 4 arguments: name, capital, region, and image filename.
+  // - The Scanner object scan is successfully initialized.
+  
+  // Write a for loop that goes through the countryArray.
+  for(int i = 0; i < countryArray.length; i++ ) {
     // Do the following inside the loop
-      String input = scan.nextLine();
-      String[] data = input.split(",");
-      System.out.println("Read in " + data[0]);
-      // inside the loop, create a new Country using your constructor with 3 arguments and pass in data[0], data[1], data[2], data[3] as arguments.
-     // inside the loop, set countryArray[i] to the created Country object
-     
+    String input = scan.nextLine();
+    String[] data = input.split(",");
+    System.out.println("Read in " + data[0]);
     
+    countryArray[i] = new Country(data[0], data[1], data[2], data[3]);
+    // inside the loop, create a new Country using your constructor with 3 arguments and pass in data[0], data[1], data[2], data[3] as arguments.
+    // inside the loop, set countryArray[i] to the created Country object
   }
 
-  /* showCountry() will show the image associated with the current country. It should get the country at index from the countryArray. It should use its get method to get its image file name and use the code below to put the image in the GUI.
-  */
-  public void showCountry() {
-    // Get the country at index from countryArray
-    
-    // Use its get method to get the its image file name and save it into imagefile variable below instead of worldmap.jpg.
-    String imagefile = "worldmap.jpg";
-    // Use the following code to create an new Image Icon and put it into the GUI
-    img = new ImageIcon(imagefile);
-    imageLabel.setIcon(img);
-  }
+  // Post-condition:
+  // - The countryArray is populated with Country objects, each created from data read from the CSV file.
+  // - Each Country in the array is initialized with values from the CSV file (name, capital, region, image filename).
+  // - The Scanner object has processed the data file, and the file has been closed (implicitly by Scanner when the method ends).
+}
+
+public void showCountry() {
+  // Get the country at index from countryArray
+  Country c = countryArray[index];
+  String imagefile = c.getImage();
+
+  // Use its get method to get the its image file name and save it into imagefile variable below instead of worldmap.jpg.
+  // Use the following code to create a new Image Icon and put it into the GUI
+  img = new ImageIcon("/workspaces/Countries/workspace/"+ imagefile);
+  imageLabel.setIcon(img);
+
+  // Pre-condition:
+  // - The index variable is within valid bounds (0 to countryArray.length - 1).
+  // - countryArray is populated with valid Country objects.
+  // - Each Country object in the array has a valid image filename (through its getImage() method).
   
-  /* nextButton should increment index. If the index is greater than 9, reset it back to 0. Clear the outputLabel to empty string using setText, and call showCountry();*/
-  public void nextButtonClick()
+  // Post-condition:
+  // - The appropriate country's image is displayed in the GUI using ImageIcon.
+  // - The image is updated on the imageLabel component in the GUI.
+}
+
+public void nextButtonClick()
+{
+  index++;
+
+  if (index > 9) 
   {
-    
-  }
-  
-  /* reviewButton should get the country at index from the countryArray, call its toString() method and save the result, print it out with System.out.println and as an argument to outputLabel.setText( text to print out ); */
-  public void reviewButtonClick()
-  {
-     
+    index = 0;
   }
 
-  /* quizButton should clear the outputLabel (outputLabel.setText to empty string), get the country at index from countryArray, print out a question about it like What country is this? and/or What's this country's capital?. Get the user's answer using scan.nextLine() and check if it is equal to the country's data using its get methods and print out correct or incorrect.
-  */
-  public void quizButtonClick()
+  outputLabel.setText("");
+
+  showCountry();
+
+  // Pre-condition:
+  // - The index variable exists and is an integer.
+  // - The countryArray has been populated with Country objects.
+  // - The outputLabel is a valid GUI component that can update its text.
+
+  // Post-condition:
+  // - The index is incremented by 1.
+  // - If the index exceeds the maximum valid index (9), it wraps around to 0.
+  // - The outputLabel is cleared by setting its text to an empty string.
+  // - showCountry() is called to update the image display based on the new index.
+}
+
+public void reviewButtonClick()
+{
+  Country c = countryArray[index];
+  String countryInfo = c.toString();
+
+  System.out.println(countryInfo);
+
+  outputLabel.setText(countryInfo);
+
+  // Pre-condition:
+  // - The index variable exists and points to a valid entry in countryArray.
+  // - countryArray has been populated with Country objects.
+  // - Each Country object implements a toString() method that returns a string representing the country's information.
+  // - outputLabel is a valid GUI component that can update its text.
+
+  // Post-condition:
+  // - The country at the current index is retrieved from the countryArray.
+  // - The country’s information is printed to the console.
+  // - The information is displayed in the outputLabel as text.
+}
+
+public void quizButtonClick()
+{
+  outputLabel.setText("");
+  Country c = countryArray[index];
+
+  if (input.getText().equalsIgnoreCase(c.getName()))
   {
-    Scanner scan = new Scanner(System.in); 
-    
-    
-    
+    System.out.println("correct!");
+    outputLabel.setText("correct!");
   }
+  else 
+  {
+    System.out.println("Incorrect! The correct answer is " + c.getName() + ".");
+    outputLabel.setText("Incorrect! The correct answer is " + c.getName() + "."); 
+  }
+
+  // Pre-condition:
+  // - The index variable exists and points to a valid entry in countryArray.
+  // - The countryArray is populated with Country objects.
+  // - outputLabel is a valid GUI component capable of displaying text.
+  // - The input (presumably a text field for user input) is available to get the user's answer.
+  // - The Country class has a getName() method to retrieve the country’s name.
+
+  // Post-condition:
+  // - The outputLabel is cleared.
+  // - The user’s input is compared to the name of the current country (from countryArray).
+  // - If the user’s answer matches the country name (case-insensitive), the output label displays "correct!".
+  // - If the user’s answer is incorrect, the output label displays "Incorrect! The correct answer is [country name]."
+  // - A corresponding message ("correct!" or "Incorrect!") is printed to the console.
+}
 
 
 
@@ -105,9 +186,15 @@ public Main() {
         // create a label to display image
         imageLabel = new JLabel(img);
         // and one for output
+        
         outputLabel = new JLabel();
         jFrame.add(imageLabel);
         jFrame.add(outputLabel);
+        
+        input = new JTextField(20);
+        jFrame.add(input);
+
+
         jFrame.setVisible(true);
         // add event listener for button click
         reviewButton.addActionListener(new ActionListener() {
